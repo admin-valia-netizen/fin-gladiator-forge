7f2179643d4d91a362c11fd4868007605a3fb8a9
admin-valia-netizen <eugeniogslsr@gmail.com>
2026-02-08 02:37:05 -0400

    Update BronzePassport.tsx

--- src/components/BronzePassport.tsx
+++ src/components/BronzePassport.tsx
@@ -292,15 +292,11 @@ export const BronzePassport = () => {
                 />
               </motion.div>
             </div>
-
-                      {/* QR Code label */}
+          {/* QR Code label */}
           <div className="text-center">
             <p className="text-[10px] text-muted-foreground font-mono mt-1">
-              {(data.registrationId || data.id).substring(0, 8).toUpperCase()}-DYN
+              {(data.registrationId || data.id || '').substring(0, 8).toUpperCase()}-DYN
             </p>
-          </div>
-
-            </div>
           </div>
 
           {/* Bronze border decoration */}
